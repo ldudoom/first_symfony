20bb654602d3b339488d1f0fc37ffdc5c99cfe4f
Raul Chauvin <raul.chauvin@telefonica.com>
2022-04-19 15:10:12 -0500

    Agregando easy admin al proyecto

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
 ];
